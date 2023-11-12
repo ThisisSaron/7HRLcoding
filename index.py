8a48b696cc0e48550afb8e234768dba1f1b81a11
ThisisSaron <135553332+ThisisSaron@users.noreply.github.com>
2023-11-12 10:26:15 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 class Parent:
@@ -7,14 +9,26 @@ class Parent:
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.rect = pygame.rect(x1,y1,x2,y2)
 
 class Player(Parent):
     def __init__(self, x1,y1,x2,y2,level):
         super().__init__(x1,y1,x2,y2)
-        player.level = level
+        self.level = level
 class Enemy(Parent):
+    def __init__(self, x1,y1,x2,y2):
+        super().__init__(x1,y1,x2,y2)
+class Objective(Parent):
     def __init__(self, x1,y1,x2,y2,level):
         super().__init__(x1,y1,x2,y2)
+        self.level = level
+
+
+
+obj_list = []
+enemy_list = []
+
+
 
 running = True
 while running:
@@ -22,9 +36,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if player.level == 0:
-        
-    if player.level == 1:
+    while player.level == 0:
+
+    while player.level == 1:
+        for obj in obj_list:
+            if pygame.Rect.colliderect(player.rect,obj.rect)
 
 
 
