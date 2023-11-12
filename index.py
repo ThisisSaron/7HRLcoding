9448a0e0193fd94a1fa6c0f0e2683e884aefcd46
ThisisSaron <135553332+ThisisSaron@users.noreply.github.com>
2023-11-12 10:44:34 -0500

    Update index.py

--- index.py
+++ index.py
@@ -4,29 +4,30 @@ import random
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 class Parent:
-    def __init__(self,x1,y1,x2,y2):
+    def __init__(self,x1,y1,x2,y2,color):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.color = color
         self.rect = pygame.Rect(self.x1,self.y1,self.x2,self.y2)
     def appear(self):
-        pass
+        pygame.draw.rect(screen,self.color,self.rect)
 
 class Player(Parent):
-    def __init__(self, x1,y1,x2,y2,level):
-        super().__init__(x1,y1,x2,y2)
+    def __init__(self, x1,y1,x2,y2,color,level):
+        super().__init__(x1,y1,x2,y2,color)
         self.level = level
 class Enemy(Parent):
-    def __init__(self, x1,y1,x2,y2):
-        super().__init__(x1,y1,x2,y2)
+    def __init__(self, x1,y1,x2,y2,color):
+        super().__init__(x1,y1,x2,y2,color)
 class Objective(Parent):
-    def __init__(self, x1,y1,x2,y2,level):
-        super().__init__(x1,y1,x2,y2)
+    def __init__(self, x1,y1,x2,y2,color,level):
+        super().__init__(x1,y1,x2,y2,color)
         self.level = level
 
 
-player = Player(0,0,32,32,1)
+player = Player(0,0,32,32,(255,0,0),1)
 obj_list = []
 enemy_list = []
 
@@ -41,6 +42,7 @@ while running:
     while player.level == 0:
         pass
     while player.level == 1:
+        player.appear()
         for obj in obj_list:
             if pygame.Rect.colliderect(player.rect,obj.rect):
                 pass
